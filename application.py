6b999d452e862ad606e63d9682dc1f4baed4cf15
NahumLa <112948696+NahumLa@users.noreply.github.com>
2022-10-11 18:00:57 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,22 @@
+from flask sqlalchemy import SQLAlchemy
+from flask import Flask
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLALCHEMY(app)
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unigue = True, nullable = False)
+    description = db.Column(db.String(120))
+    
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+    
+@app.route('/')
+def index():
+    return 'Hello!'
+
+@app.route('/drinks')
+def get_drinks():
+    return {'drinks': 'drink data'}
